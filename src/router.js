ab9d98ab3ff982573c629b9d29bc0bc759e40af7
straydoggie <straydoggie@163.com>
2019-03-30 03:38:47 +0800

    deleted all comments in store.js

--- src/router.js
+++ src/router.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 //import Home from './views/Home.vue'
+//component: () => import('./views/About.vue')
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'hash',
   base: process.env.BASE_URL,
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  //component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
   routes: [
     {
       path: '/',
--- src/store.js
+++ src/store.js
@@ -37,47 +37,12 @@ export default new Vuex.Store({
           state.editing = state.papers[i];
         }
       }
-      console.log(state.editing);
     },
     addToPaper(state, payload) {
-      //
-      console.log("addToPaper => identifier: " + payload.identifier);
-      console.log("addToPaper => before: " + state.editing.content[payload.type]);
       state.editing.content[payload.type].unshift(payload.identifier);
-      console.log("addToPaper => after: " + state.editing.content[payload.type]);
-      return
-      /* switch (type) {
-        case "sc":
-          state.editing.content.sc.unshift(identifier);
-          break;
-        case "mc":
-          state.editing.content.mc.unshift(identifier);
-          break;
-        case "fb":
-          state.editing.content.fb.unshift(identifier);
-          break;
-        case "qt":
-          state.editing.content.qt.unshift(identifier);
-          break;
-        case "st":
-          state.editing.content.st.unshift(identifier);
-          break;
-        case "sa":
-          state.editing.content.sa.unshift(identifier);
-          break;
-        case "eq":
-          state.editing.content.eq.unshift(identifier);
-          break;
-        default:
-          console.log("unknow type: " + type);
-          break;
-      } */
     },
     removeFromPaper(state, payload) {
-      console.log("removeFromPaper => before: " + state.editing.content[payload.type]);
       state.editing.content[payload.type].splice(payload.number, 1);
-      console.log("removeFromPaper => after: " + state.editing.content[payload.type]);
-      return;
     },
     publishPaper() {
       //
@@ -112,13 +77,10 @@ export default new Vuex.Store({
         .then(response => console.log(response.data));
     },
     selectItem(context, identifier) {
-      console.log("---------- selectItem ----------");
       let operated = false;
       Object.keys(context.state.editing.content).forEach(function (k) {
         for (let i = 0; i < context.state.editing.content[k].length; i++) {
-          console.log("selectItem => forEachKey: " + k + "." + i);
           if (context.state.editing.content[k][i] === identifier) {
-            console.log("selectItem => removeFromPaper: " + identifier);
             context.commit("removeFromPaper", { type: k, number: i });
             operated = true;
             break;
@@ -126,12 +88,8 @@ export default new Vuex.Store({
         }
       });
       if (operated) return operated;
-      console.log("item is not existed in paper");
       for (let i = 0; i < context.state.library.length; i++) {
-        console.log(context.state.library.length + " => " + i);
         if (context.state.library[i].identifier === identifier) {
-          console.log("item is found in library, type is " + context.state.library[i].type);
-          console.log("selectItem => addToPaper: " + identifier);
           context.commit("addToPaper", { type: context.state.library[i].type, identifier: identifier });
           operated = true;
           break;
